4a4606c3727d2c4a746528c9340a36658f493f54
Yelnur Seitzhanov <yelnur.seitzhanov@aspose.com>
2023-08-02 18:45:17 +0600

    production mode configured

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 const { Sequelize } = require('sequelize');
 const dbConf = require('./config')
 let sequelize;
-if(process.env === "production") {
+
+if(process.env.NODE_ENV === "production") {
+  console.log("here")
   sequelize = new Sequelize(dbConf.production.database, dbConf.production.username, dbConf.production.password, {
     host: dbConf.production.host,
     dialect: dbConf.production.dialect,
     port: dbConf.production.port
   });
 } else {
+  console.log("111")
   sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
     host: dbConf.development.host,
     dialect: dbConf.development.dialect,
