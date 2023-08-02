d3acab158ce526bb480cdf61c8ff022cd01650e5
Yelnur Seitzhanov <yelnur.seitzhanov@aspose.com>
2023-08-02 18:41:23 +0600

    production mode configured

--- config/config.js
+++ config/config.js
@@ -7,6 +7,11 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    // конфигурация для продакшн среды
+    username: 'doadmin',
+    password: 'AVNS_R5m2RT4GNnFkGK9Qs09',
+    database: 'defaultdb',
+    host: 'db-postgresql-sgp1-13396-do-user-14463706-0.b.db.ondigitalocean.com',
+    dialect: 'postgres',
+    port: 25060
   },
 };
